0978f9096651ca4fae89cac998db6bd3184fdf94
Harsh Yadav <harshrao812@gmail.com>
2025-09-26 20:25:27 +0000

    feat(auth): implement sign-in and sign-up views with validation
    Add username field to user collection and update payload types
    feat(auth): create TRPC procedures for authentication and session management
    feat(ui): integrate Sonner for toast notifications and add form components

---
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SingInView } from "@/modules/auth/ui/views/sign-in-view"
+
+const Page = () => {
+  return (
+    <div>
+      <SingInView />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,127 @@
+'use client'
+
+import z from "zod"
+import Link from "next/link"
+import { Poppins } from "next/font/google"
+import { useForm } from "react-hook-form"
+import { loginSchema } from "../../schemas"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from "@tanstack/react-query"
+
+import { toast } from "sonner"
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { Input } from "@/components/ui/input"
+import { useTRPC } from "@/trpc/client"
+import { useRouter } from "next/navigation"
+
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['700'],
+})
+
+export const SingInView = () => {
+  const router = useRouter()
+  const trpc = useTRPC()
+  const login = useMutation(trpc.auth.login.mutationOptions({
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: () => {
+      router.push("/")
+    }
+  }))
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    mode: 'all',
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  })
+
+  const onSumbit = (values: z.infer<typeof loginSchema>) => {
+    login.mutate(values)
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-5">
+      <div className="bg-[#f4f4f0] h-screen w-full lg:col-span-3 overflow-y-auto">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSumbit)}
+            className="flex flex-col gap-8 p-4 lg:p-16">
+            <div className="flex items-center justify-between mb-8">
+              <Link href={"/"}>
+                <span className={cn("text-2xl font-semibold  ", poppins.className)}>
+                  Shopsy
+                </span>
+              </Link>
+              <Button
+                asChild
+                variant="ghost"
+                size="sm"
+                className="text-base border-none underline"
+              >
+                <Link
+                  prefetch
+                  href={"/sign-up"}>
+                  Sign Up
+                </Link>
+              </Button>
+            </div>
+            <h1 className="text-4xl font-medium">
+              Welcome back to Shopsy
+            </h1>
+
+            <FormField
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              disabled={login.isPending}
+              type="submit"
+              size={"lg"}
+              variant={"hoverElevated"}
+              className="bg-black text-white hover:bg-blue-400 hover:text-primary"
+            >
+              Log In
+            </Button>
+          </form>
+        </Form>
+      </div>
+      <div className="h-screen w-full lg:col-span-2 hidden lg:block"
+        style={{
+          backgroundImage: "url('/auth-bg.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}>
+
+      </div>
+    </div>
+  )
+}
