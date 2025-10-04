0226df48ce10af193d50dfcefcec6466c09772e0
Harsh Yadav <harshrao812@gmail.com>
2025-10-04 16:50:30 +0000

    feat: integrate Stripe account verification and enhance order management

---
+++ src/components/stripe-verify.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button, Link } from "@payloadcms/ui"
+
+export const StripeVerify = () => {
+  return (
+    <Link href='/stripe-verify'>
+      <Button>
+        Verify account
+      </Button>
+    </Link>
+  )
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_BG_COLOR = '#f5f5f5'
 
 export const DEFAULT_LIMIT = 8
+
+export const PLATFORM_FEE_PERCENTAGE = 10
