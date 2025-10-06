28e876ac19297a389ca56e3aa8b4d780acd17ab1
Harsh Yadav <harshrao812@gmail.com>
2025-10-06 17:51:40 +0000

    removes unused imports

--- src/app/(app)/(library)/library/page.tsx
+++ src/app/(app)/(library)/library/page.tsx
@@ -2,10 +2,8 @@ import { DEFAULT_LIMIT } from "@/constants"
 import { LibraryView } from "@/modules/library/ui/views/library-view"
 import { caller, getQueryClient, trpc } from "@/trpc/server"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
-import { headers } from "next/headers"
 import { redirect } from "next/navigation"
-import { getPayload } from "payload"
-import config from '@payload-config'
+
 
 export const dynamic = 'force-dynamic'
 
